f7febd5379d039c3f4b821ed88b94fb59461436b
bekabdimanasov <bekabdimanasov@gmail.com>
2021-01-03 12:51:34 +0300

    Moved 'fortune cookie' functionality into module

--- meadowlark.js
+++ meadowlark.js
@@ -17,17 +17,10 @@ app.get('/', function(req, res) {
   res.render('home')
 })
 
-const fortunes = [
-  "Победи свои страхи, или они победят тебя.",
-  "Рекам нужны истоки.",
-  "Не бойся неведомого.",
-  "Тебя ждет приятный сюрприз.",
-  "Будь проще везде, где только можно."
-]
+const fortune = require('./lib/fortune.js')
 // Пользовательская страница About Page
 app.get('/about', function(req, res) {
-  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-  res.render('about', {fortune: randomFortune})
+  res.render('about', {fortune: fortune.getFortune()})
 })
 
 // Пользовательская страница 404 (промежуточное ПО)
